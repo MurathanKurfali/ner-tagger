0cd2db8bf164da3223182ea9bbab5210116e67b9
murathankurfali <murathankurfali@gmail.com>
2023-12-23 11:54:44 +0100

    evaluation updated, new results added

--- alvis.sh
+++ alvis.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#SBATCH --gpus-per-node=V100:2
+#SBATCH --gpus-per-node=A40:4
 #SBATCH --nodes=1
-#SBATCH -t 06:00:0
+#SBATCH -t 24:00:00
 #SBATCH --output=log-%j.out
 #SBATCH -A NAISS2023-22-1238
-#SBATCH --exclude=alvis3-14
 
 module load CUDA/12.3.0
 module load Python/3.11.3-GCCcore-12.3.0
 source rise_env/bin/activate
-./run.sh $1 $2 $3
+./run.sh "$1" "$2" "$3" "$4" "$5"
--- scripts/evaluate_predictions.py
+++ scripts/evaluate_predictions.py
@@ -8,24 +8,57 @@ import pandas as pd
 # Read the gold annotations from the jsonl file
 def read_gold_data(file_path):
     gold_tags = []
+    gold_tokens = []  # To store tokens
     with open(file_path, 'r') as file:
         for line in file:
             entry = json.loads(line)
             gold_tags.append(entry['ner_tags'])
-    return gold_tags
+            gold_tokens.append(entry['tokens'])  # Extract tokens
+    return gold_tags, gold_tokens
+
+
+def filter_unseen_tokens(training_tokens, training_tags,test_tokens, test_tags, test_predictions):
+    unseen_tokens_predictions = defaultdict(list)
+
+    # Flatten the training tokens list for easier membership checking
+
+    training_tokens_flat = [token for instance in training_tokens for token in instance]
+    training_tags_flat = [tag for instance in training_tags for tag in instance]
+    seen_token_tag_pairs = set(list(zip(training_tokens_flat, training_tags_flat)))
+
+    # learn a masking to mask out the (tag,token) pairs that are in the training data too
+    mask_out_predictions = []
+    for tokens, tags in zip(test_tokens, test_tags):
+        sentence_mask = []
+        # for each (token,tag) in a sentence, check if they are in the training data
+        for token, tag in zip(tokens, tags):
+            if (token, tag) not in seen_token_tag_pairs:
+                sentence_mask.append(True)
+            else:
+                sentence_mask.append(False)
+        mask_out_predictions.append(sentence_mask)
+
+    # apply the mask and leave the unseen tokens in the test data
+    masked_test_tags = [[token for token, keep in zip(tokens, mask) if keep] for tokens, mask in zip(test_tags, mask_out_predictions)]
+    masked_test_predictions = {}
+    for prediction_file, predictions in test_predictions.items():
+        masked_test_predictions[prediction_file] = [[token for token, keep in zip(tokens, mask) if keep] for tokens, mask in zip(predictions, mask_out_predictions)]
+
+    return masked_test_tags, masked_test_predictions
 
 # The fine-tuning process saves the predictions of the model in
 # "predictions.txt" file.
-def read_predictions(dir_path):
+def read_predictions(dir_path, transformer_model):
     predictions = defaultdict(list)
     for root, dirs, files in os.walk(dir_path):
         for file in files:
-            if file.endswith('predictions.txt'):
+            if file.endswith('predictions.txt') and transformer_model in root:
                 with open(os.path.join(root, file), 'r') as f:
                     file_predictions = f.readlines()
                     for line in file_predictions:
                         predictions[f].append(line.strip().split())
     return predictions
+
 
 # # Calculates scores for each tag using the 'evaluate' and seqeval library
 def calculate_scores_per_tag(true_labels, true_predictions):
@@ -59,6 +92,7 @@ if __name__ == "__main__":
     parser.add_argument('saved_model_dir', type=str, help='Directory of saved model')
     parser.add_argument('gold_data_dir', type=str, help='Directory of gold data')
     parser.add_argument('transformer_model', type=str, help='Transformer model name')
+    parser.add_argument('only_unseen', type=bool, default=True, help='Transformer model name')
     parser.add_argument('output_format', type=str, default='html', nargs='?', choices=['csv', 'html', "markdown"],
                         help='Output format for the results (default: html)')
     args = parser.parse_args()
@@ -67,11 +101,19 @@ if __name__ == "__main__":
         predictions_dir_path = os.path.join(args.saved_model_dir, f"system_{system_version}")
         gold_data_path = f"{args.gold_data_dir}/system_{system_version}/test.json"
 
-        gold_tags = read_gold_data(gold_data_path)
-        predictions = read_predictions(predictions_dir_path)
+        test_tags, test_tokens = read_gold_data(gold_data_path)
+        predictions = read_predictions(predictions_dir_path, args.transformer_model)
         run_count = len(predictions)
-        scores_dict = calculate_scores_per_tag(gold_tags, predictions)
+        assert run_count > 0, print("No predictions.txt is found. Make sure you entered the correct path/ transformer model name")
         output_file_name = f"system-{system_version}_{args.transformer_model}_{run_count}.{args.output_format}"
+
+        if args.only_unseen:
+            training_gold_data_path = gold_data_path.replace("test", "train")
+            training_tags, training_tokens = read_gold_data(training_gold_data_path)
+            test_tags, predictions = filter_unseen_tokens(training_tokens,training_tags,test_tokens, test_tags, predictions)
+            output_file_name = output_file_name.replace(f".{args.output_format}", f"-unseen.{args.output_format}")
+
+        scores_dict = calculate_scores_per_tag(test_tags, predictions)
 
         df = pd.DataFrame.from_dict(scores_dict, orient='index').T
 
